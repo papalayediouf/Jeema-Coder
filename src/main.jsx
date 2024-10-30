1ca7ba34ae392a0d3e9a5a52f97c6761949d4d84
Papa Abdoulaye Diouf <papalayediouf17@gmail.com>
2024-10-30 16:18:48 +0000

    Solution commit

--- src/main.jsx
+++ src/main.jsx
@@ -2,12 +2,12 @@ import { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
 import './index.css'
 
-import MonFormulaire from './component/Formulaire.jsx'
-import App from './App.jsx'
+// import MonFormulaire from './component/Formulaire.jsx'
+import Form from './Form.jsx'
 
 createRoot(document.getElementById('root')).render(
   <StrictMode>
-    <App/>
-    <MonFormulaire/>
+    <Form/>
+    {/* <MonFormulaire/> */}
   </StrictMode>,
 )
